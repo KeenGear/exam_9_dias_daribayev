3ac7140be7ac87349c3c9ea32b91b3d94f56f1cf
Dias Daribayev <fission.frames-0s@icloud.com>
2023-04-22 11:57:03 +0600

    Инит комит CRUD галереи

--- source/app/urls.py
+++ source/app/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('gallery_app.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- source/gallery_app/views.py
+++ source/gallery_app/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.edit import FormMixin, UpdateView, DeleteView
+
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
 
 from .models import Post
 
@@ -10,3 +16,52 @@ class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
     context_object_name = 'posts'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.user = self.request.user
+        post.save()
+        return super().form_valid(form)
+
+
+class PostDetailView(FormMixin, DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
